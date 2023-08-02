c8ea42ecd5b5bc55f7535e8af66a378423af9da7
nitin kukreti <nitin.k@binmile.com>
2023-08-02 14:28:36 +0530

    use strict variables datatype methods of primitives number string arrays arrays methods

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+// x = 3.14;  
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date();
+
+let rohan={
+    name:"rohan",
+    sayHi:()=> alert("Hi buddy!"),
+
+}
+rohan.sayHi()
+
+
+
+
+
